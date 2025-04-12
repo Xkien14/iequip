9375bc416ba441a692c6d82d47e8cbdcff0f8343
kien <kenperalta18@gmail.com>
2025-04-12 21:04:05 +0800

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,42 @@
+export const navLinks = [
+	{
+		label: "Dashboard",
+		url: "/",
+		icon: "/icons/gauge-solid.svg",
+	},
+	{
+		label: "Items",
+		url: "/items",
+		icon: "/icons/screwdriver-wrench-solid.svg",
+	},
+	{
+		label: "Borrow",
+		url: "/borrow",
+		icon: "/icons/handshake-angle-solid.svg",
+	},
+	{
+		label: "Consumables",
+		url: "/supplies",
+		icon: "/icons/product.png",
+	},
+	{
+		label: "Consum.. Trans",
+		url: "/supply-transactions",
+		icon: "/icons/transactions.png",
+	},
+	{
+		label: "User",
+		url: "/user",
+		icon: "/icons/group.png",
+	},
+	{
+		label: "Activity Log",
+		url: "/activity-log",
+		icon: "/icons/activity-log.png",
+	},
+	{
+		label: "Guides",
+		url: "/guide",
+		icon: "/icons/info.png",
+	},
+];
